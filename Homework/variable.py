9596d5f275fb9a64c18e534b5505061539aa2c3c
Denis.K <dk.ptspb@ya.ru>
2024-09-04 20:18:26 +0300

    ДЗ "Переменные"

---
+++ Homework/variable.py
@@ -0,0 +1,5 @@
+home_works_count = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / home_works_count
+print('Курс:', course_name + ', всего задач:', str(home_works_count) + ', затрачено часов:', str(number_of_hours_spent) + ', среднее время выполнения', time_for_one_task, 'часа.')
