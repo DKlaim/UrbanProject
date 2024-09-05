8c276cbddb8b5fa1fc0adebc14130e99f7f0bb94
Denis.K <dk.ptspb@ya.ru>
2024-09-05 15:13:09 +0300

    ДЗ: "Организация программ и методы строк"

---
+++ Homework/module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Ввод новой строки: ')
+
+print('Строка содержит', len(my_string), 'символов.')
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка без пробелов:', my_string.replace(' ', ''))
+print('Первый символ строки:', my_string[0])
+print('Последний символ строки:', my_string[-1])
