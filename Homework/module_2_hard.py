31f499850ff0042265a9980ed76131fa4a541727
Denis.K <dk.ptspb@ya.ru>
2024-09-14 02:38:46 +0300

    Дополнительное ДЗ: "Слишком древний шифр" mod_7

--- Homework/module_2_hard.py
+++ Homework/module_2_hard.py
@@ -10,26 +10,10 @@ def couples(value):
     for i in range(1, value):
         for j in range(1, value):
             if value % (i + j) == 0 and (i + j) > 2:
-                couples_list += str(i) + '/' + str(j) + ' '  # Разделители для более наглядной демонстрации
+                couples_list += f'{i}{j}'
     return couples_list
 
 
 # Вывод результата
-def result(number):
-    print(f'Число в первой ячейке: {number}')
-    print(f'Пары: {couples(number)}')
-    print('Пароль:', couples(number).replace('/', '').replace(' ', ''))
-
-
-# Вывод решения задачи
-result(random_number)
-
-# # Проверяем вывод пар для любого числа больше 2
-# n = 3
-# while n >= 3:
-#     print('-------------------- ПРОВЕРКА --------------------')
-#     n = int(input('Введите любое число начиная с 3 или меньшее для выхода: '))
-#     if n >= 3:
-#         result(n)
-#     else:
-#         break
+print(f'Число в первой ячейке: {random_number}')
+print(f'Пароль: {couples(random_number)}')
