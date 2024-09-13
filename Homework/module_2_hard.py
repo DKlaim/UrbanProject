3e2b0d5b280e1f85d5f47a8d5dd773aa75d08208
Denis.K <dk.ptspb@ya.ru>
2024-09-14 01:25:26 +0300

    Дополнительное ДЗ: "Слишком древний шифр" mod_5

--- Homework/module_2_hard.py
+++ Homework/module_2_hard.py
@@ -1,35 +1,36 @@
 import random
 
-
-# Создаём список чисел от 1 до 21
-def numeric_list():
-    num_list = []
-    for i in range(1, 21):
-        num_list.append(i)
-    return num_list
+# Дано
+number_list = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+random_number = random.choice(number_list)
 
 
-# Получаем случайное число в первой ячейке из диапазона от 3 до 20
-first_window = random.choice(numeric_list()[2:])
+# Делаем выборку всех возможных пар чисел для любого числа больше 3
+def couples(value):
+    couples_list = ''
+    for i in range(1, value):
+        for j in range(1, value):
+            if value % (i + j) == 0 and (i + j) > 2:
+                couples_list += str(i) + '/' + str(j) + ' '  # Разделители для более наглядной демонстрации
+    return couples_list
 
 
-# Получаем пары для случайного числа
-def couples(first_win, num_set):
-    couples_ = ''
-    n = 0
-    k = 1
-    # Делаем выборку всех возможных пар чисел для удовлетворения условия задачи
-    while n < first_win:
-        for j in range(k, first_win):
-            if first_win % (num_set[n] + num_set[j]) == 0:
-                couples_ += str(num_set[n]) + '/' + str(
-                    num_set[j]) + ' '  # Разделители для более наглядной демонстрации
-        n += 1
-        k = 0
-    return couples_
+# Вывод результата
+def result(number):
+    print(f'Число в первой ячейке: {number}')
+    print(f'Пары: {couples(number)}')
+    print('Пароль:', couples(number).replace('/', '').replace(' ', ''))
 
 
-couples = (couples(first_window, numeric_list()))
-print(f'Число в первой ячейке: {first_window}')
-print(f'Пары: {couples}')
-print('Пароль:', couples.replace('/', '').replace(' ', ''))
+# Вывод решения задачи
+result(random_number)
+
+# Проверяем вывод пар для любого числа больше 3
+n = 3
+while n >= 3:
+    print('-------------------- ПРОВЕРКА --------------------')
+    n = int(input('Введите любое число от 3 до 20 или 0 для выхода: '))
+    if n >= 3:
+        result(n)
+    else:
+        break
