3c7b29db0a44caf3966ae3c9bccafd675d8467fe
Denis.K <dk.ptspb@ya.ru>
2024-09-19 19:01:46 +0300

    Вебинар по Модулю-2

---
+++ Lessons_2/module_2_webinar.py
@@ -0,0 +1,56 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#
+# primes = []
+# not_primes = []
+#
+# for number in numbers:
+#     if number == 1:
+#         continue
+#
+#     is_print = True
+#     for divider in range(2, number // 2 + 1):
+#         if number % divider == 0:
+#             is_print = not is_print
+#             break
+#     if is_print:
+#         primes.append(number)
+#     else:
+#         not_primes.append(number)
+#
+# print(primes)
+# print(not_primes)
+# print()
+#
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#
+# primes = []
+# not_primes = []
+#
+# for number in numbers:
+#     if number == 1:
+#         continue
+#
+#     # a = range(2, number // 2 + 1)  # пример как отрабатывает range (смотреть в дебаге)
+#     for divider in range(2, number // 2 + 1):
+#         if number % divider == 0:
+#             not_primes.append(number)
+#             break
+#     else:
+#         primes.append(number)
+#
+# print(primes)
+# print(not_primes)
+
+
+code = int(input('Type you http code: '))
+
+if code == 404:
+    print()
+elif
+    print()
+elif
+    print()
+elif
+    print()
+
+
