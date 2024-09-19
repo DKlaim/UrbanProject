a7c39d6a1f1e1bbb437a21471c631942fb14f3df
Denis.K <dk.ptspb@ya.ru>
2024-09-19 23:41:40 +0300

    Mod_3 "Практика по функциям" - дополнения

--- Lessons_3/func_pract1.py
+++ Lessons_3/func_pract1.py
@@ -1,3 +1,6 @@
+my_list = [11, 2, 3, 6, 9, 8, 7, 10, 4, 5, 5, 4, 3, 1]
+print(my_list)
+
 def find_max(list_):
     max_ = list_[0]
     for i in list_:
@@ -5,9 +8,8 @@ def find_max(list_):
             max_ = i
     return max_
 
-my_list = [11, 2, 3, 6, 9, 8, 7, 10, 4, 5]
-print(find_max(my_list))  # Функция find_max сравнивает все элементы списка друг с другом и выдаёт значение максимального элемента
-
+# Функция find_max сравнивает все элементы списка друг с другом и выдаёт значение максимального элемента
+print(f'Максимальное число из списка: {find_max(my_list)}')
 
 def count_event(list_):
     counter = 0
@@ -16,4 +18,15 @@ def count_event(list_):
             counter += 1
     return counter
 
-print(count_event(my_list))  # Функция count_event сравнивает все элементы списка друг с другом и выдаёт значение максимального элемента
+# Функция count_event сравнивает все элементы списка друг с другом и выдаёт количество чётных чисел в списке
+print(f'Количество чётных чисел в списке: {count_event(my_list)}')
+
+def unique(list_):
+    new_list = []
+    for i in list_:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
+print(f'Список уникальных элементов: {unique(my_list)}')  # Функция unique выбирает из имеющегося списка неповторяющиеся элементы и создаёт список уникальных элементов
