9d2e34ec69147a654c19e65c2f43a36e15ad2f0f
Denis.K <dk.ptspb@ya.ru>
2024-09-27 18:41:32 +0300

    Мод4_Л4: "Практика. Часть 2" - Пакет с функциями для сортировки

---
+++ Lessons_4/arrays.py
@@ -0,0 +1,20 @@
+from SortFunc.sotrfunc import insertion_sort, bubble_sort, selection_sort
+
+data_1 = [9, 5, 6, 4, 8, 7, 3, 2, 1]
+data_2 = ['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'b', 'v', 'c', 'x', 'z']
+data_3 = ['9', '5', '6', '4', '8', '7', '3', '2', '1', 'q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'b', 'v', 'c', 'x', 'z']
+
+
+print(insertion_sort(data_1))
+print(insertion_sort(data_2))
+print(insertion_sort(data_3))
+print()
+
+print(bubble_sort(data_1))
+print(bubble_sort(data_2))
+print(bubble_sort(data_3))
+print()
+
+print(selection_sort(data_1))
+print(selection_sort(data_2))
+print(selection_sort(data_3))
