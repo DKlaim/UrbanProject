ecbbec35d8d75bd50139ae61df8b9f079649c744
Denis.K <dk.ptspb@ya.ru>
2024-10-01 21:09:25 +0300

    Мод_3_ДЗ_5: Задача "Рекурсивное умножение цифр" - добавлена проверка нуля в конце числа

--- Homework/module_3_5.py
+++ Homework/module_3_5.py
@@ -4,10 +4,10 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return first
+        return first if first != 0 else 1
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
 
 
@@ -16,7 +16,7 @@ print(result)
 #     str_number = str(number)
 #     first = int(str_number[0])
 #     if len(str_number) == 1:
-#         return first
+#         return first if first != 0 else 1
 #     else:
 #         return first * get_multiplied_digits(int(str_number[1:]))
 #
