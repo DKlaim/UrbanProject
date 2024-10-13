ad1220302c7cf1bc5e3c74f26b29face0df12ebe
Denis.K <dk.ptspb@ya.ru>
2024-10-13 20:04:23 +0300

    Мод5_Практика_ч3: Попытка реализовать регистрацию и авторизацию в отдельном классе

---
+++ Lessons_5/user_registration_2.py
@@ -0,0 +1,62 @@
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, username, password):
+        self.data[username] = password
+
+
+class User:
+    # Строка ниже - строка документирования, которая содержит описание класса (вывод документации: User.__doc__)
+    """
+    Класс пользователя, содержащий атрибуты username и password, а также отвечает за регистрацию и авторизацию
+    """
+
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+    def registration(*self):
+        print('Регистрация пользователя:')
+        while True:
+            user = User(input('Придумайте логин: '), password := input('Придумайте пароль: '),
+                        password_confirm := input('Для подтверждения повторите ввод пароля: '))
+            if password != password_confirm:
+                print()
+                print('Ошибка при повторном введении пароля! Попробуйте ещё раз.')
+                continue
+            else:
+                database.add_user(user.username, user.password)
+                print()
+                print(f'Пользователь {user.username} успешно создан.')
+                print(database.data)
+                print()
+                break
+
+    def authorization(database):
+        print('Авторизация пользователя:')
+        while True:
+            username = input('Введите логин: ')
+            if username != ' ' and username in database:
+                password = input('Введите пароль: ')
+                if password == database[username]:
+                    print()
+                    print(f'Авторизация пользователя {username} успешно выполнена.')
+                    break
+                else:
+                    print()
+                    print('Неверный пароль! Попробуйте ещё раз.')
+                    continue
+            elif username == ' ':
+                break
+            else:
+                print()
+                print(f'Пользователь {username} не найден. Попробуйте ещё раз или введите пробел для выхода.')
+                continue
+
+
+if __name__ == '__main__':
+    database = Database()
+    User.registration()
+    User.authorization(database.data)
