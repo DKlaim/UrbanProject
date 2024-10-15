b6b737947888dbb6c4df64f6e83999bb8a895409
Denis.K <dk.ptspb@ya.ru>
2024-10-15 17:19:31 +0300

    ДЗ mod_5_hard: Задание "Свой YouTube" v1.1

--- Homework/module_5_hard.py
+++ Homework/module_5_hard.py
@@ -1,25 +1,33 @@
 class UrTube:
     def __init__(self):
-        self.users = {}
-        self.videos = {}
+        self.users = []
+        self.videos = []
         self.current_user = None
 
     def register(self, nickname, password, age):
-        if nickname in self.users:
-            print(f'Пользователь {nickname} уже существует')
+        if self.users:
+            list_usernames = []
+            for user in self.users:
+                list_usernames += user
+            if nickname in list_usernames:
+                print(f'Пользователь {nickname} уже существует')
+            else:
+                self.users += [{nickname: [password, age]}]
+                self.log_in(nickname, password)
         else:
-            self.users[nickname] = [password, age]
+            self.users += [{nickname: [password, age]}]
             self.log_in(nickname, password)
 
     def log_in(self, nickname, password):
-        if nickname in self.users and hash(self.users[nickname][0]) == hash(password):
-            self.current_user = nickname
+        for user in self.users:
+            if nickname in user and hash(user[nickname][0]) == hash(password):
+                self.current_user = nickname
 
     def log_out(self):
         self.current_user = None
 
-    def add(self, *args):
-        pass
+    def add(self, *other):
+        self.videos += other
 
     def get_videos(self):
         pass
@@ -53,15 +61,33 @@ class User:
 
 
 if __name__ == '__main__':
-    # user = User('den', 123123, 23)
-    # print(user)
-    # print(hash(user))
-    # print(user.age)
-    # v1 = Video('Лучший язык программирования 2024 года', 200)
-    # v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+    ur = UrTube()
+    # user1 = User('den', 123123, 23)
+    # user2 = User('max', 321123, 22)
+    # print(user1)
+    # print(hash(user1))
+    # print(user1.age)
+    ur.register('den', 123123, 23)
+    # print()
+    # print(user2)
+    # print(hash(user2))
+    # print(user2.age)
+    ur.register('max', 321123, 22)
+    ur.register('den', 123123, 23)
+    # print()
+    print(ur.users)
+    print()
+
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
     # print(v1)
     # print(v2)
-    ur = UrTube()
-    ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-    print(ur.current_user)
-    print(hash(ur.current_user))
+    # print()
+    # ur.add(v1, v2)
+    # print(ur.videos)
+
+    # ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+    # print(ur.current_user)
+    # print(hash(ur.current_user))
+    # ur.add(v1, v2)
+    # print(ur.videos)
