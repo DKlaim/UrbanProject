f916897cf37358199908dba7f53b33d175ba9fd1
Denis.K <dk.ptspb@ya.ru>
2024-10-21 21:56:38 +0300

    ДЗ mod_6_2: Задача "Изменять нельзя получать"

--- Homework/module_6_2.py
+++ Homework/module_6_2.py
@@ -1,20 +1,20 @@
 class Vehicl:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, model, color, __engine_power):
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
         self.owner = owner
-        self.model = model
-        self.color = color
-        self.__engine_power = __engine_power
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
 
     def get_model(self):
-        return f'Модель: {self.model}'
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
         return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        return f'Цвет: {self.color}'
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
         print(self.get_model())
@@ -25,7 +25,7 @@ class Vehicl:
     def set_color(self, new_color):
         color_variants = [color.lower() for color in self.__COLOR_VARIANTS]
         if new_color.lower() in color_variants:
-            self.color = new_color
+            self.__color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
@@ -48,3 +48,4 @@ if __name__ == '__main__':
 
     # Проверяем что поменялось
     vehicle1.print_info()
+    # print(dir(vehicle1))
