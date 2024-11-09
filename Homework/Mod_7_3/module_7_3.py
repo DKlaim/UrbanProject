68cc7679ee9c0cb0dcaeb0507f183f63ed83f4c3
Denis.K <dk.ptspb@ya.ru>
2024-11-09 14:40:51 +0300

    ДЗ mod_7_3: Задача "Найдёт везде"

--- Homework/Mod_7_3/module_7_3.py
+++ Homework/Mod_7_3/module_7_3.py
@@ -1,24 +1,33 @@
 class WordsFinder:
     def __init__(self, *files):
         self.file_names = str(files)[1:-1].replace(",", '').replace("'", '').split(' ')
-        # print(self.file_names)
 
     def get_all_words(self):
         all_words = {}
+        list_words = []
         for file_name in self.file_names:
             with open(file_name, encoding='utf-8') as file:
                 for line in file:
                     for symbol in [',', '.', '=', '!', '?', ';', ':', ' - ', ' — ']:
-                        line = line.replace(symbol, '')
-                    print(line.lower(), end='')
+                        line = line.lower().replace(symbol, '')
+                    list_words += line.split()
+            all_words[file_name] = list_words
+            list_words = []
+        return all_words
 
     def find(self, word):
-        pass
+        for name, words in self.get_all_words().items():
+            if word.lower() in words:
+                return {name: words.index(word.lower()) + 1}
 
     def count(self, word):
-        pass
+        for name, words in self.get_all_words().items():
+            if word.lower() in words:
+                return {name: words.count(word.lower())}
 
 
 if __name__ == '__main__':
-    finder2 = WordsFinder('test_file.txt, Captain.txt')
-    finder2.get_all_words()
+    finder2 = WordsFinder('test_file.txt')
+    print(finder2.get_all_words())  # Все слова
+    print(finder2.find('TEXT'))  # 3 слово по счёту
+    print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
