ebfe444ca313a405ec7b2ef40df433308588972f
Denis.K <dk.ptspb@ya.ru>
2024-11-10 01:49:32 +0300

    Мод7: Работа с файлами и форматированный вывод. Л5: Форматирование строк

---
+++ Lessons_7/format_strings.py
@@ -0,0 +1,4 @@
+print('Привет, ' + 'Мир!')
+print('Меня зовут %(name)s, мне %(year)s' %{'name': 'Денис', 'year': 40})
+print('Я учусь в {title}{postfix} {title}'.format(title='Urban', postfix='-University'))
+print(f'{'Urban'} это лучший Университет!')
