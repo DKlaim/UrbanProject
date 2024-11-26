a8b2857165d47f776bbc98654e3a5ac03fb42ec7
Denis.K <dk.ptspb@ya.ru>
2024-11-26 19:31:50 +0300

    ДЗ mod_10_1: "Введение в потоки" Задача "Потоковая запись в файлы" - Alpha

---
+++ Homework/Mod_10_1/module_10_1.py
@@ -0,0 +1,40 @@
+import threading
+import time
+from time import sleep
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for n in range(1, word_count + 1):
+            file.write(f'Какое-то слово № {n}\n')
+
+    print(f'Завершилась запись в файл {file_name}')
+    sleep(0.1)
+
+
+started_at = time.time()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+ended_at = time.time()
+
+elapsed = round(ended_at - started_at, 4)
+print(f'Функции завершили свою работу за {elapsed} секунд(ы)')
+
+flow_1 = threading.Thread(target=write_words(10, 'example5.txt'))
+flow_2 = threading.Thread(target=write_words(30, 'example6.txt'))
+flow_3 = threading.Thread(target=write_words(200, 'example7.txt'))
+flow_4 = threading.Thread(target=write_words(100, 'example8.txt'))
+
+started_at = time.time()
+flow_1.start()
+flow_2.start()
+flow_3.start()
+flow_4.start()
+ended_at = time.time()
+
+elapsed = round(ended_at - started_at, 4)
+print(f'Потоки завершили свою работу за {elapsed} секунд(ы)')
+
+print(threading.enumerate())
