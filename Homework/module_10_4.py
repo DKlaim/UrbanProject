8e5f635d23f4230f28da12a61354e457cd7da2a9
Denis.K <dk.ptspb@ya.ru>
2024-12-02 16:47:46 +0300

    ДЗ mod_10_4: "Очереди для обмена данными между потоками" Задача "Потоки гостей в кафе" - Alpha

---
+++ Homework/module_10_4.py
@@ -0,0 +1,53 @@
+import threading
+from random import randint
+from time import sleep
+from queue import Queue
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+
+class Guest(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        sleep(randint(3, 10))
+
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = tables
+
+    def guest_arrival(self, *guests):
+        # for guest in guests:
+        #     print(guest)
+        table = self.queue.get()
+        for guest in guests:
+            print(f'{guest} сел(-а) за стол номер {table}')
+
+    def discuss_guests(self):
+        pass
+
+
+if __name__ == '__main__':
+    # Создание столов
+    tables = [Table(number) for number in range(1, 6)]
+    # Имена гостей
+    guests_names = [
+        'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+        'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+    ]
+    # Создание гостей
+    guests = [Guest(name) for name in guests_names]
+    # Заполнение кафе столами
+    cafe = Cafe(*tables)
+    # Приём гостей
+    cafe.guest_arrival(*guests)
+    # # Обслуживание гостей
+    # cafe.discuss_guests()
