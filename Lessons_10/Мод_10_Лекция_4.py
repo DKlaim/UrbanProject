b86f45c7a15addf600bac2128612282e045ca300
Denis.K <dk.ptspb@ya.ru>
2024-12-04 14:29:30 +0300

    Мод_10_Лекция_4: "Очереди в потоках" - дополнения в комментах

--- Lessons_10/Мод_10_Лекция_4.py
+++ Lessons_10/Мод_10_Лекция_4.py
@@ -7,17 +7,17 @@ import threading
 from time import sleep
 
 
-def getter(queue):
-    while True:
+def getter(queue):  # функция принимает очередь, для дальнейшего получения элементов из очереди
+    while True:  # бесконечный цикл с таймаутом 5 сек, который будет пытаться получить элемент из очереди (while not queue.empty() - конструкция работающая пока очередь не пустая)
         sleep(5)
         item = queue.get()  # метод get получает элемент из очереди, параметром timeout можно указать время сколько ждать
-        print(f'{threading.current_thread()} взял элемент {item}')
+        print(f'{threading.current_thread()} взял элемент {item}')  # операции с элементами
 
 
 q = Queue(maxsize=10)  # пустая очередь (объект класса Queue), maxsize ограничивает количество элементов в очереди
 
-thread_1 = threading.Thread(target=getter, args=(q,), daemon=True)
-thread_1.start()
+thread_1 = threading.Thread(target=getter, args=(q,), daemon=True)  # target обрабатывает функцию getter, args принимает очередь, daemon=True активируем как демона для завершения работы потока по отработке основного потока
+thread_1.start()  # запуск потока
 
 for i in range(10):
     sleep(2)
