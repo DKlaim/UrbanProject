5a4f24c6e1eda71b116a29dee69e520d890cfb55
Denis.K <dk.ptspb@ya.ru>
2024-12-10 02:25:00 +0300

    ДЗ mod_10_4: "Очереди для обмена данными между потоками" Задача "Потоки гостей в кафе"

--- Homework/module_10_4.py
+++ Homework/module_10_4.py
@@ -29,7 +29,8 @@ class Cafe:
             for table in self.tables:
                 if table.guest is None:
                     table.guest = guest.name
-                    guest.start()
+                    Guest(table.guest).start()
+                    # guest.start()
                     print(f'{table.guest} сел(-а) за стол номер {table.number}')
                     break
 
@@ -37,23 +38,18 @@ class Cafe:
                 self.queue.put(guest.name)
                 print(f'{guest.name} в очереди')
 
-        print(threading.enumerate())  # проверка потоков
-
     def discuss_guests(self):
         while not self.queue.empty() or None not in [table.guest for table in self.tables]:
             for table in self.tables:
-                # if Guest(table.guest).is_alive():
+                if not Guest(table.guest).is_alive():
                     print(f'{table.guest} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {table.number} свободен')
                     table.guest = None
 
-            if not self.queue.empty() and None in [table.guest for table in self.tables]:
-                for table in self.tables:
-                    if table.guest is None:
-                        table.guest = self.queue.get()
-                        print(f'{self.queue.get()} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
-                        Guest(table.guest).start()
-
+                if not self.queue.empty() and table.guest is None:
+                    table.guest = self.queue.get()
+                    print(f'{table.guest} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                    Guest(table.guest).start()
 
 
 if __name__ == '__main__':
