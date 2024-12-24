caec5504bb684d6c5422446524740145da3b3a06
Denis.K <dk.ptspb@ya.ru>
2024-12-24 11:40:18 +0300

    Мод_11_Лекция_3: "Виртуальное окружение"

---
+++ Lessons_11/Мод_11_Лекция_3.py
@@ -0,0 +1,3 @@
+''' Виртуальное окружение Python '''
+# Установка - <папка окружения> python -m venv "название окружения (обычно venv)"
+# Активация - 1. Windows: ..\venv\Scripts\activate.bat 2. Linux: source ../venv/bin/activate
