f148fe43568828e6f8585670c97fa88e6ef5aa65
Denis.K <dk.ptspb@ya.ru>
2025-01-04 11:54:16 +0300

    Мод_12_Лекция_3: "Методы Юнит-тестирования"

--- Lessons_12/Unittest_Calc/test_calc.py
+++ Lessons_12/Unittest_Calc/test_calc.py
@@ -1,8 +1,31 @@
+# Понятия и термины в Юнит-тестировании:
+# Фикстура - это отдельный метод в котором реализовано определённое тестовое действие и вывод результата.
+# Тест-Кейс - это отдельный класс с набором действий/тестов/фикстур.
+# Тест-Сьют - это отдельный класс с набором Тест-Кейсов для объединения тестов в группы.
+# Тест-ранер - компонент, который отвечает за запуск тестирования и всю логику/структуру тестирования.
 import calc
 import unittest
 
 
 class CalcTest(unittest.TestCase):
+    def setUp(self):  # метод setUp - функция, которая выполняется перед каждой фикстурой
+        print('UpSetup')
+    # setUp - то что происходит перед каждым кейсом
+
+    @classmethod
+    def setUpClass(cls):  # метод setUpClass - функция, которая выполняется 1 раз в самом начале запуска теста (используется с декоратором @classmethod)
+        print('UpMegaSetup')
+    # setUpClass - то что происходит перед каждым тест-кейсом
+
+    def tearDown(self):  # метод tearDown - функция, которая выполняется после каждой фикстуры
+        print('DownSetup')
+    # tearDown - то что происходит после каждого кейса
+
+    @classmethod
+    def tearDownClass(cls):  # метод tearDownClass - функция, которая выполняется 1 раз в самом конце теста (используется с декоратором @classmethod)
+        print('DownMegaSetup')
+    # tearDownClass - то что происходит после каждого тест-кейса
+
     def test_add(self):
         """
         Test for add function in calculator
@@ -30,6 +53,16 @@ class CalcTest(unittest.TestCase):
         :return:
         """
         self.assertEqual(calc.div(2, 2), 1)
+
+    def test_test(self):  # Другие функции сравнения
+        # self.assertIsNone('')  # Проверка на пустоту
+        self.assertIsNotNone('abc')  # Проверка на не пустоту
+        self.assertIn('a', 'abc')   # Проверка на содержание
+        self.assertTrue(True)   # Проверка на Истину
+        self.assertFalse(False)   # Проверка на Лож
+        # self.assertRaises(1 / 0)   # Проверка на вызов ошибки
+        self.assertAlmostEquals(0.0000001, 0.0000001)   # Сравнение с точностью до 7 знака после точки
+        # А также больше/меньше и т.п.
 
 
 if __name__ == '__main__':
