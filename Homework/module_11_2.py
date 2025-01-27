56dd55f73407554c2498147c86e5be1896b31236
Denis.K <dk.ptspb@ya.ru>
2025-01-27 21:13:05 +0300

    ДЗ module_11_2: "Интроспекция"

--- Homework/module_11_2.py
+++ Homework/module_11_2.py
@@ -4,12 +4,10 @@ def introspection_info(obj):
     info['type'] = type(obj).__name__
     info['attributes'] = dir(obj)
     info['methods'] = [attr for attr in dir(obj) if callable(getattr(obj, attr))]
-    info['module'] = getattr(obj, "__module__", None)
+    info['module'] = __name__
 
     return info
 
 number_info = introspection_info(42)
 print(number_info)
 
-any_obj = introspection_info(introspection_info)
-print(any_obj)
