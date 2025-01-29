14d2bdd7a717bb12d26376b2ad248625bb8b963d
Denis.K <dk.ptspb@ya.ru>
2025-01-29 18:39:28 +0300

    Мод11_Вебинар: Черепашка

---
+++ Lessons_11/Webinar/my_turtle.py
@@ -0,0 +1,39 @@
+import turtle
+
+turtle.showturtle()
+# turtle.forward(100)  # движение вперёд
+# turtle.backward(200)  # движение назад
+# turtle.left(15)  # поворот влево на х градусов от текущего положения
+# turtle.right(15)  # поворот вправо на х градусов от текущего положения
+# turtle.setheading(15)  # поворот на х градусов от нулевого положения
+# turtle.setheading(-15)  # поворот на х градусов от нулевого положения
+# turtle.shape('turtle')  # изменение внешнего вида (turtle, square, arrow, circle)
+turtle.Screen().setup(800, 600)  # параметры окна
+turtle.hideturtle()
+turtle.goto(-120, 120)
+turtle.write('Сверху')
+turtle.goto(50, -120)
+turtle.write('Снизу')
+turtle.goto(100, 20)
+turtle.write('Справа')
+turtle.goto(-100, 0)
+turtle.write('Слева')
+
+
+# turtle.forward(100)
+# turtle.backward(200)
+# turtle.right(15)
+# turtle.forward(100)
+# turtle.backward(200)
+# turtle.left(15)
+# turtle.forward(100)
+# turtle.backward(200)
+# turtle.right(15)
+# turtle.forward(100)
+# turtle.backward(200)
+# turtle.left(15)
+# turtle.forward(100)
+# turtle.backward(200)
+# turtle.right(15)
+# turtle.forward(100)
+# turtle.backward(200)
